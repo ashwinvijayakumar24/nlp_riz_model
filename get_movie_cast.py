eeaa30c896452fad024afa01672f5c14127262a3
Ashwin Vijayakumar <ashwinvijayakumar@ashwins-mac.local>
2025-05-29 16:29:07 -0500

    prototype completed

--- get_movie_cast.py
+++ get_movie_cast.py
@@ -3,16 +3,29 @@ import pandas as pd
 
 ia = imdb.Cinemagoer()
 
+# https://www.boxofficeindia.com/worldwide-total-gross.php?year=2004
 movie_dict = {
-    2004: ['Veer Zaara', 'Main Hoon Na', "Dhoom"], 
-    2005: ['Bunty Aur Babli', 'Mangal Pandey', 'Sarkar'],
-    2006: ['Dhoom 2', 'Don', 'Fanaa'],
-    2007: ['Om Shanti Om', 'Chak De India', 'Taare Zameen Par'],
-    2008: ['Jodhaa Ahkbar', 'Sarkar Raj', 'Thoda Pyaar Thoda Magic'],
-    2009: ['3 Idiots', 'Ajab Prem Ki Ghazab Kahani', 'Wanted'],
-    2010: ['My Name is Khan', 'Tees Maar Khan', 'Once Upon a Time in Mumbaai'],
-    2011: ['Don 2', 'Zindagi Na Milegi Dobara', 'Rockstar'],
-    2012: ['Ek Tha Tiger', 'Jab Tak Hai Jaan', 'Bol Bachchan'],
+    # the top 5 highest-grossing worldwide Bollywood movies as per boxofficeindia.com will not include movies released in languages other than hindi
+    # 2004: ['Veer Zaara', 'Main Hoon Na', "Mujhse Shaadi Karogi", "Dhoom", "Khakee"], 
+    # 2005: ['No Entry', 'Bunty Aur Babli', 'Saalam Namaste', 'Garam Masala', 'Mangal Pandey -  The Rising'],
+    # 2006: ['Dhoom 2', 'Krrish', 'Lage Raho Munnabhai', 'Kabhi Alvida Naa Kehna', 'Don - The Chase Begins Again'],
+    # 2007: ['Om Shanti Om', 'Welcome', 'Chak de India', 'Partner', 'Taare Zameen Par'],
+    # 2008: ['Ghajini', 'Rab Ne Bana Di Jodi', 'Singh is Kinng', 'Jodhaa Akbar', 'Race'],
+    # 2009: ['3 Idiots', 'Love Aaj Kal', 'Ajab Prem Ki Ghazab Kahani', 'Wanted', 'Kambakkht Ishq'],
+    # 2010: ['Dabangg', 'My Name is Khan', 'Golmaal 3', 'Raajneeti', 'Housefull'],
+    # 2011: ['Bodyguard', 'Ra.One', 'Don 2', 'Ready', 'Zindagi Na Milegi Dobara'],
+    # 2012: ['Ek Tha Tiger', 'Dabangg 2', 'Jab Tak Hai Jaan', 'Rowdy Rathore', 'Agneepath'],
+    # 2013: ['Dhoom 3', 'Chennai Express', 'Yeh Jawaani Hai Deewani', 'Krrish 3', 'Goliyon Ki Raasleela Ram - Leela'],
+    # 2014: ['PK','Kick', 'Happy New Year', 'Bang Bang', 'Singham Returns'],
+    # 2015: ['Bajrangi Bhaijaan', 'Dilwale', 'Prem Ratan Dhan Payo', 'Bajirao Mastani', 'Tanu Weds Manu Returns'],
+    # 2016: ['Dangal', 'Sultan', 'Ae Dil Hai Mushkil', 'Rustom', 'Airlift'],
+    # 2017: ['Tiger Zinda Hai', 'Golmaal Again', 'Raees', 'Tubelight', 'Judwaa 2'],
+    # 2018: ['Sanju', 'Padmaavat', 'Simmba', 'Race 3', 'Baaghi 2'],
+    # 2019: ['War', 'Kabir Singh', 'Uri - The Surgical Strike', 'Bharat', 'Good Newwz'],
+    # 2020: ['Tanhaji - The Unsung Warrior', 'Baaghi 3', 'Street Dancer 3D', 'Malang', 'Shubh Mangal Zyada Saavdhan'],
+    # 2021: ['Sooryavanshi', '83', 'Antim: The Final Truth', 'Bell Bottom', 'Tadap'],
+    # 2022: ['Brahmastra - Part One - Shiva', 'Bhool Bhulaiyaa 2', 'The Kashmir Files', 'Gangubai Kathiawadi', 'Laal Singh Chaddha'],
+    # 2023: ['Jawan', 'Animal', 'Pathaan', 'Gadar 2', 'Tiger 3']
 }
 
 movie_cast_data = []
@@ -24,10 +37,18 @@ for year, movies in movie_dict.items():
             if not search_results:
                 print(f"Movie not found: {movie_title} ({year})")
                 continue
-            if movie_title == "Dhoom" or movie_title == "Sarkar" or movie_title == "Don":
+            if movie_title == "Dhoom" or movie_title == "Kick":
                 movie_id = search_results[1].movieID
+            elif movie_title == "Race" or movie_title == "Bodyguard" or movie_title == "Ready" or movie_title == "Bang Bang":
+                movie_id = search_results[3].movieID
             elif movie_title == "Wanted":
                 movie_id = search_results[4].movieID
+            elif movie_title == "Partner":
+                movie_id = search_results[5].movieID
+            elif movie_title == "Welcome":
+                movie_id = search_results[6].movieID
+            elif movie_title == "War":
+                movie_id = search_results[9].movieID           
             else:
                 movie_id = search_results[0].movieID
 
--- issues.md
+++
@@ -1,6 +0,0 @@
-## Issues
-- bol bachchan/agneepath
-- weight different riz criteria 
-
-## Bollywood Hungama updates:
-
